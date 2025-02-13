195f72dcae637a1691a54d4c503d7c18d03b9408
hesham badr <heshammoha231992@gmail.com>
2025-02-13 12:30:20 +0200

    يششسيش

--- example/components/Sidebar/sidebar.tsx
+++ example/components/Sidebar/sidebar.tsx
@@ -1,10 +1,8 @@
 import classNames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import cookie from "cookie";
 import jwt from "jsonwebtoken";
-import { NextPageContext } from "next";
-import React, { useState, useMemo, useCallback, useEffect } from "react";
+import { useEffect, useState, useCallback, useMemo } from "react";
 import {
   ArticleIcon,
   CollapsIcon,
@@ -15,8 +13,9 @@ import {
   VideosIcon,
 } from "../../../components/icons";
 import ReportsIcon from "components/icons/reports";
-
 import { jwtDecode } from "jwt-decode";
+import { useSidebar } from "utils/sidebarcontext";
+
 interface MenuItem {
   id: number;
   label: string;
@@ -27,13 +26,15 @@ interface MenuItem {
 const menuItems: MenuItem[] = [
   { id: 1, label: "الرئيسية", icon: HomeIcon, link: "/admin/home" },
   { id: 3, label: "التقارير", icon: ReportsIcon, link: "/admin/reports" },
-  // { id: 4, label: "Manage Tutorials", icon: VideosIcon, link: "/tutorials" },
 ];
 
 const Sidebar = (props) => {
-  const [toggleCollapse, setToggleCollapse] = useState(false);
+  const { toggleCollapse, setToggleCollapse } = useSidebar();
   const [isCollapsible, setIsCollapsible] = useState(false);
   const router = useRouter();
+  const [image, setImage] = useState();
+  const [info, setInfo] = useState();
+  const [role, setRole] = useState();
 
   // Memoize active menu item based on the current route
   const activeMenu = useMemo(
@@ -70,13 +71,11 @@ const Sidebar = (props) => {
 
   const handleLogout = async () => {
     try {
-      // Make a POST request to the logout API route
       const response = await fetch("/api/logout", {
         method: "POST",
       });
 
       if (response.status == 200) {
-        // Redirect to the login page after successful logout
         router.push("/admin/login");
       } else {
         console.error("Logout failed");
@@ -87,25 +86,16 @@ const Sidebar = (props) => {
   };
 
   const onMouseOver = useCallback(() => {
-    setIsCollapsible(!isCollapsible);
-  }, [isCollapsible]);
+    setIsCollapsible(!toggleCollapse);
+  }, [toggleCollapse]);
 
   const handleSidebarToggle = useCallback(() => {
-    const newToggleState = !toggleCollapse;
-    setToggleCollapse(newToggleState);
-    // Save the new state to localStorage
-    localStorage.setItem("sidebarCollapsed", JSON.stringify(newToggleState));
-  }, [toggleCollapse]);
-  const [image, setImage] = useState();
-  const [info, setInfo] = useState();
-  const [role, setRole] = useState();
+    setToggleCollapse((prevState) => !prevState);
+  }, [setToggleCollapse]);
+
   // On component mount, check localStorage for saved state
   useEffect(() => {
     if (!localStorage.getItem("token")) router.push("/admin/login");
-    const savedState = localStorage.getItem("sidebarCollapsed");
-    if (savedState !== null) {
-      setToggleCollapse(JSON.parse(savedState));
-    }
     try {
       const token = localStorage.getItem("token");
       const info = jwtDecode(token);
@@ -130,7 +120,7 @@ const Sidebar = (props) => {
             <img
               src={image}
               alt="Profile"
-              className="rounded-full w-24 h-24 object-cover" // Circular profile image
+              className="rounded-full w-24 h-24 object-cover"
             />
             <span
               className={classNames(
@@ -143,15 +133,16 @@ const Sidebar = (props) => {
               Welcome {info}
             </span>
           </div>
-          {isCollapsible && (
-            <button
-              aria-label="Collapse Sidebar"
-              className={collapseIconClasses}
-              onClick={handleSidebarToggle}
-            >
-              <CollapsIcon />
-            </button>
-          )}
+          {/* Show collapse button only when sidebar is expanded */}
+          {/* {!toggleCollapse && ( */}
+          <button
+            aria-label="Collapse Sidebar"
+            className={collapseIconClasses}
+            onClick={handleSidebarToggle}
+          >
+            <CollapsIcon />
+          </button>
+          {/* )} */}
         </div>
 
         <div className="flex flex-col items-start mt-24">
@@ -175,11 +166,10 @@ const Sidebar = (props) => {
             );
           })}
         </div>
-        {role == "admin" && (
+        {role === "admin" && (
           <Link href="/admin/addadmin">
             <a className="flex py-4 px-3 items-center justify-center w-full h-full">
               <div style={{ width: "2.5rem", color: "white" }}>
-                {/* <Icon /> */}
                 <ArticleIcon fill="white" />
               </div>
               {!toggleCollapse && (
@@ -215,31 +205,5 @@ const Sidebar = (props) => {
     </div>
   );
 };
-export default Sidebar;
 
-export async function getServerSideProps(context: NextPageContext) {
-  const { req, res } = context;
-  try {
-    const isAuthenticated = req.cookies.authToken ? true : false;
-    if (!isAuthenticated) {
-      // Redirect the user to login page before rendering the component
-      return {
-        redirect: {
-          destination: "/admin/login", // Redirect URL
-          permanent: false, // Set to true if you want a permanent redirect
-        },
-      };
-    }
-    const user = jwt.verify(req.cookies.authToken, "rawaesecret");
-    return {
-      props: { user }, // Pass user data as props
-    };
-  } catch (error) {
-    return {
-      redirect: {
-        destination: "/admin/login", // Redirect URL
-        permanent: false, // Set to true if you want a permanent redirect
-      },
-    };
-  }
-}
+export default Sidebar;
