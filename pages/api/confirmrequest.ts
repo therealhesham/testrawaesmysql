2450462d75756a28d3f2ae1ab951531093a4fda6
hesham badr <heshammoha231992@gmail.com>
2025-04-23 00:56:45 +0200

    Add JWT authentication and logging for order confirmation and updates

--- pages/api/confirmrequest.ts
+++ pages/api/confirmrequest.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { PrismaClient } from "@prisma/client";
+import jwt from "jsonwebtoken";
+
 const prisma = new PrismaClient();
 
 export default async function handler(
@@ -26,6 +28,26 @@ export default async function handler(
         Order: { connect: { id } },
       },
     });
+
+    try {
+      const token = req.cookies?.authToken;
+      let userId: string | null = null;
+
+      if (token) {
+        const decoded: any = jwt.verify(token, "rawaesecret");
+        userId = decoded?.username;
+      }
+
+      await prisma.logs.create({
+        data: {
+          Status: `  تم تأكيد الطلب رقم ${waiter?.id}   للعميل  ${waiter?.ClientName}  `,
+          homemaidId: waiter?.HomemaidId,
+          userId: userId,
+        },
+      });
+    } catch (error) {
+      console.error("Error updatin logs:", error);
+    }
 
     // Return the updated order with the newly created arrival
     res.status(200).json(waiter);
