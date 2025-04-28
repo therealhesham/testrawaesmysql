c5ab30ad1dc92200fa17d96b1674ec3d04a93dac
hesham badr <heshammoha231992@gmail.com>
2025-04-29 00:30:23 +0300

    S

--- example/containers/Layout.tsx
+++ example/containers/Layout.tsx
@@ -51,6 +51,7 @@ function Layout({ children }: ILayout) {
         <nav className="bg-white shadow-lg" dir="rtl">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
+              {/* Logo Section */}
               <div className="flex">
                 <div className="flex-shrink-0 flex items-center">
                   <Link href="/admin/home">
@@ -59,7 +60,9 @@ function Layout({ children }: ILayout) {
                     </span>
                   </Link>
                 </div>
-                <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
+
+                {/* Navigation Links */}
+                <div className="hidden sm:mr-6 sm:flex sm:space-x-8">
                   <Link href="/admin/home">
                     <span className="border-transparent text-yellow-500 hover:border-yellow-600 hover:text-yellow-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium">
                       الرئيسية
