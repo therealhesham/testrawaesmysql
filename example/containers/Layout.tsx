126500c50241719ae1fb5c84d3145684a6459e11
heshambadr <your.email@example.com>
2025-04-30 00:37:53 -0700

    logo

--- example/containers/Layout.tsx
+++ example/containers/Layout.tsx
@@ -82,7 +82,11 @@ function Layout({ children }: ILayout) {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between h-16">
               <div className="flex items-center">
-                {/* Logo Section */}
+               <img
+  src="/images/coloredlogo.png"
+  className="h-10 w-10 object-contain"
+  alt="لوجو روائس"
+/>
                 <div className="flex-shrink-0">
                   <Link href="/admin/home">
                     <span className="text-2xl font-bold text-yellow-600">
