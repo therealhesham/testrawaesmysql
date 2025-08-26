39c392457ac7a76fbb2fa05ad3cf10dbc5a83411
your.email@example.com <your.email@example.com>
2025-08-26 14:52:09 +0300

    feat: Add external departures data fetching and integrate with the home page

---
+++ pages/api/homeinitialdata/deparaturefromsaudi.ts
@@ -0,0 +1,41 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+      const deparaturesCount = await prisma.arrivallist.count({
+      where: {
+        DeparatureFromSaudiDate: { not: null },
+      }});
+    const deparatures = await prisma.arrivallist.findMany({
+      where: {
+        DeparatureFromSaudiDate: { not: null },
+      },
+           select: {
+        Order: { select: { Name: true ,HomemaidId: true,HomeMaid:true} },
+        OrderId: true,
+        
+        SponsorName: true,
+        PassportNumber: true,
+        deparatureDate: true,
+        deparatureTime: true,
+        SponsorPhoneNumber: true,
+        HomemaidName: true,
+        id: true,
+      },
+      take: 3, 
+      orderBy: { id: "desc" },
+    });
+    res.status(200).json({data:deparatures,deparaturesCount});
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Error fetching data" });
+  } finally {
+    // Disconnect Prisma Client regardless of success or failure
+    await prisma.$disconnect();
+  }
+}
